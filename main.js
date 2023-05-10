d11b433c703d8d361447149e2a620fe51f5474a3
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2023-05-10 14:20:03 +0200

    Add invalid style, remove input error message ASAP

--- main.js
+++ main.js
@@ -6,14 +6,24 @@ const formError = document.querySelector('form + span.error');
 function showError() {
   if (email.validity.typeMismatch || email.value.length < 1) {
     emailError.textContent = 'Please enter a valid email address';
+    email.className = 'invalid';
   }
 }
 
-email.addEventListener('focusout', () => {
+// Perform first check when user exits the input
+email.addEventListener('focusout', (e) => {
   if (email.validity.valid) {
     emailError.textContent = '';
-    emailError.className = 'error';
-  } else showError();
+    email.classList.remove('invalid');
+  } else showError(e);
+});
+
+// Remove invalid message and style as soon as the input is valid
+email.addEventListener('input', () => {
+  if (email.validity.valid) {
+    emailError.textContent = '';
+    email.classList.remove('invalid');
+  }
 });
 
 form.addEventListener('submit', (e) => {
